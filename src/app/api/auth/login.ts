f7c07ad58bc30906c370b8de71ecb249e64f697a
Nahuel Santillan <nahueltecnicomdp@gmail.com>
2023-07-31 00:02:42 -0300

    a

--- src/app/api/auth/login.ts
+++ src/app/api/auth/login.ts
@@ -1,7 +1,3 @@
-import { prisma } from "@/database/client";
-
-export async function GET() {
-  const users = await prisma.user.findMany();
-
-  return new Response(JSON.stringify(users));
+export async function POST() {
+  return new Response("Login");
 }
--- src/app/api/hello/route.ts
+++ src/app/api/hello/route.ts
@@ -1,7 +1,3 @@
-import { prisma } from "@/database/client";
-
 export async function GET() {
-  const users = await prisma.user.findMany();
-
-  return new Response(JSON.stringify(users));
+  return new Response("Hi there");
 }
